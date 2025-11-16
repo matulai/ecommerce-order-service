109e13cf5a84ab7297a283d9f68a493f01869caf
Matias Laime <matulai1221@gmail.com>
2025-11-15 23:41:19 -0300

    Add: order service

---
+++ src/main/java/com/matulai/order_service/service/OrderService.java
@@ -0,0 +1,28 @@
+package com.matulai.order_service.service;
+
+import com.matulai.order_service.repository.OrderRepository;
+import com.matulai.order_service.dto.OrderRequest;
+import com.matulai.order_service.model.Order;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+    }
+}
