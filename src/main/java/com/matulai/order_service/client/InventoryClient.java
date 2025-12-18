4e71714d8c6122e20ba3e625fc5412ce5b7eef92
Matias Laime <matulai1221@gmail.com>
2025-12-17 23:45:45 -0300

    Add: circuit breaker with resillence4j on endpoint to service inventory

--- src/main/java/com/matulai/order_service/client/InventoryClient.java
+++ src/main/java/com/matulai/order_service/client/InventoryClient.java
@@ -1,10 +1,19 @@
 package com.matulai.order_service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
 
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        System.out.println("Cannot get inventory for skucode {" + code + "}, failure reason: {" + throwable.getMessage() + "}");
+        return false;
+    }
 }
--- src/main/java/com/matulai/order_service/config/RestClientConfig.java
+++ src/main/java/com/matulai/order_service/config/RestClientConfig.java
@@ -4,6 +4,7 @@ import com.matulai.order_service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -16,8 +17,13 @@ public class RestClientConfig {
 
     @Bean
     public InventoryClient inventoryClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(3000);
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(factory)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
